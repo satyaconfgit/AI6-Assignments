71c0f10d24591b7024e44dd09dbc059e65c902c8
satyaconfgit <38681481+satyaconfgit@users.noreply.github.com>
2018-08-11 15:23:18 +0530

    Update git_example.py

--- git_example.py
+++ git_example.py
@@ -8,11 +8,11 @@ import numpy as np
 
 def sigmoid(x):
   # enter code below
-  return (1 / (1 + numpy.exp(-x)))
+  return (1 / (1 + np.exp(-x)))
 
 def tanh(x):
-  # enter code below
+  return (np.exp(x) - np.exp(-x) )/( np.exp(x) + np.exp(-x))
   
 def relu(x):
-  # entr code below
+  return max(0, x)
   
